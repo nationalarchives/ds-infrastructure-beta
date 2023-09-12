e95913468fffc9a21c75ce6179cf29b754165115
karlkern <karl.kern@nationalarchives.gov.uk>
2023-09-12 12:08:36 +0100

    create function in python

--- components/terraform/lambda/private-beta-docker-deployment/source/private-beta-docker-deployment.py
+++ components/terraform/lambda/private-beta-docker-deployment/source/private-beta-docker-deployment.py
@@ -1,33 +1,34 @@
 import boto3
 import datetime
 
-script = """
-echo "Hello World!" > /home/ec2-user/helloworld.txt
-pwd >> /home/ec2-user/helloworld.txt
-"""
-#Define the tag possessed by the EC2 instances that we want to execute the script on
-tag = 'private-beta-dw'
+def private_beta_docker_deployment(event, context):
+    script = """
+    echo "Hello World!" > /home/ec2-user/helloworld.txt
+    pwd >> /home/ec2-user/helloworld.txt
+    """
+    #Define the tag possessed by the EC2 instances that we want to execute the script on
+    tag = 'private-beta-dw'
 
-ec2_client = boto3.client("ec2", region_name='eu-west-2')
-ssm_client = boto3.client('ssm')
+    ec2_client = boto3.client("ec2", region_name='eu-west-2')
+    ssm_client = boto3.client('ssm')
 
-filtered_instances = ec2_client.describe_instances(Filters=[{'Name': 'tag:Name', 'Values': [tag]}])
-reservations = filtered_instances['Reservations']
+    filtered_instances = ec2_client.describe_instances(Filters=[{'Name': 'tag:Name', 'Values': [tag]}])
+    reservations = filtered_instances['Reservations']
 
-exec_list=[]
-for reservation in reservations:
-    for instance in reservation['Instances']:
-        print(instance['InstanceId'], " is ", instance['State']['Name'])
-        if instance['State']['Name'] == 'running':
-            exec_list.append(instance['InstanceId'])
+    exec_list=[]
+    for reservation in reservations:
+        for instance in reservation['Instances']:
+            print(instance['InstanceId'], " is ", instance['State']['Name'])
+            if instance['State']['Name'] == 'running':
+                exec_list.append(instance['InstanceId'])
 
-    print("**************")
+        print("**************")
 
-response = ssm_client.send_command(
-    DocumentName ='AWS-RunShellScript',
-    Parameters = {'commands': [script]},
-    InstanceIds = exec_list
-)
+    response = ssm_client.send_command(
+        DocumentName ='AWS-RunShellScript',
+        Parameters = {'commands': [script]},
+        InstanceIds = exec_list
+    )
 
-#See the command run on the target instance Ids
-print(response['Command']['Parameters']['commands'])
+    #See the command run on the target instance Ids
+    print(response['Command']['Parameters']['commands'])
