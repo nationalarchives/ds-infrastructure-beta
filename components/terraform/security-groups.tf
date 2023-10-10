4d5afac02cf1c0d14f230b1a14423332f6cafa48
karlkern <karl.kern@nationalarchives.gov.uk>
2023-10-10 20:31:30 +0100

    add value

--- components/terraform/security-groups.tf
+++ components/terraform/security-groups.tf
@@ -4,26 +4,26 @@ module "sgs" {
     vpc_id = data.aws_ssm_parameter.vpc_id.value
 
     rp_lb_cidr = [
-        data.aws_ssm_parameter.public_subnet_2a_id,
-        data.aws_ssm_parameter.public_subnet_2b_id,
+        data.aws_ssm_parameter.public_subnet_2a_id.value,
+        data.aws_ssm_parameter.public_subnet_2b_id.value,
     ]
 
     rp_instance_cidr = [
-        data.aws_ssm_parameter.private_subnet_2a_id,
-        data.aws_ssm_parameter.private_subnet_2b_id,
-        data.aws_ssm_parameter.client_vpc_cidr,
+        data.aws_ssm_parameter.private_subnet_2a_id.value,
+        data.aws_ssm_parameter.private_subnet_2b_id.value,
+        data.aws_ssm_parameter.client_vpc_cidr.value,
     ]
 
     dw_lb_cidr = [
-        data.aws_ssm_parameter.private_subnet_2a_id,
-        data.aws_ssm_parameter.private_subnet_2b_id,
-        data.aws_ssm_parameter.client_vpc_cidr,
+        data.aws_ssm_parameter.private_subnet_2a_id.value,
+        data.aws_ssm_parameter.private_subnet_2b_id.value,
+        data.aws_ssm_parameter.client_vpc_cidr.value,
     ]
 
     dw_instance_cidr = [
-        data.aws_ssm_parameter.private_subnet_2a_id,
-        data.aws_ssm_parameter.private_subnet_2b_id,
-        data.aws_ssm_parameter.client_vpc_cidr,
+        data.aws_ssm_parameter.private_subnet_2a_id.value,
+        data.aws_ssm_parameter.private_subnet_2b_id.value,
+        data.aws_ssm_parameter.client_vpc_cidr.value,
     ]
 
     tags = local.tags
