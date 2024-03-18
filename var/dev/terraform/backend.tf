1ff77220b265432e6b15d3c7787f458b886faa79
karlkern <karl.kern@nationalarchives.gov.uk>
2024-03-18 09:09:02 +0000

    fix backend name

--- var/dev/terraform/backend.tf
+++ var/dev/terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
     backend "s3" {
-        bucket = "ds-terraform-state-beta-eu-west-2-846769538626"
+        bucket = "ds-terraform-state-eu-west-2-846769538626"
         key    = "ds-infrastructure-beta/terraform.tfstate"
         region = "eu-west-2"
     }
