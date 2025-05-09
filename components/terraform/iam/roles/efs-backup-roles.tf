7e7807c2aee3b19344766a382a1b596b790c0421
Vaishnavi Reddy Putluru <vaishnavireddyputluru@Vaishnavis-MacBook-Pro.local>
2025-05-09 15:19:12 +0100

    Reconciliation

--- components/terraform/iam/roles/efs-backup-roles.tf
+++ components/terraform/iam/roles/efs-backup-roles.tf
@@ -2,7 +2,9 @@ resource "aws_iam_role" "media_efs_backup" {
     name = "beta-media-efs-backup-role"
 
     assume_role_policy  = file("${path.root}/templates/efs_backup_assume_role.json")
-    managed_policy_arns = [
-        "arn:aws:iam::aws:policy/service-role/AWSBackupServiceRolePolicyForBackup"
-    ]
+}
+
+resource "aws_iam_role_policy_attachment" "media_efs_backup_policy_attachment" {
+    role       = aws_iam_role.media_efs_backup.name
+    policy_arn = "arn:aws:iam::aws:policy/service-role/AWSBackupServiceRolePolicyForBackup"
 }
