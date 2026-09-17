4e9649ed69be2bb2c8dba1f20ddc59f39db8d286
Steven Hirschorn <steve.hirschorn@nationalarchives.gov.uk>
2026-09-17 08:18:16 +0100

    Push updated tf version and provider pins

--- components/terraform/cloudfront/cloudfront.tf
+++ components/terraform/cloudfront/cloudfront.tf
@@ -72,7 +72,7 @@ resource "aws_cloudfront_distribution" "beta" {
 }
 
 resource "aws_cloudwatch_log_delivery_source" "beta" {
-#  region = "us-east-1"
+  region = "us-east-1"
 
   name         = "beta"
   log_type     = "ACCESS_LOGS"
@@ -80,7 +80,7 @@ resource "aws_cloudwatch_log_delivery_source" "beta" {
 }
 
 resource "aws_cloudwatch_log_delivery_destination" "beta" {
-#  region = "us-east-1"
+  region = "us-east-1"
 
   name          = "s3-destination"
   output_format = "w3c"
@@ -91,7 +91,7 @@ resource "aws_cloudwatch_log_delivery_destination" "beta" {
 }
 
 resource "aws_cloudwatch_log_delivery" "beta" {
-#  region = "us-east-1"
+  region = "us-east-1"
 
   delivery_source_name     = aws_cloudwatch_log_delivery_source.beta.name
   delivery_destination_arn = aws_cloudwatch_log_delivery_destination.beta.arn
